c03d2fb78c8475e1c8fa30ef39b2bfb6f78a5382
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-08-04 23:12:12 +0530

    Fix Vercel Django configuration

--- online_voting/wsgi.py
+++ online_voting/wsgi.py
@@ -14,3 +14,13 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_voting.settings')
 
 application = get_wsgi_application()
+
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "online_voting.settings"
+)
+
+application = get_wsgi_application()
